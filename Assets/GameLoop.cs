ca449c20ff5b3da84ae3e0d2bba8710229c754fd
Rodion Rasulov <rodion9687@gmail.com>
2025-05-10 14:53:10 +0100

    bursts in waves, enemies spawn on different sides of map

--- Assets/GameLoop.cs
+++ Assets/GameLoop.cs
@@ -7,7 +7,10 @@ public class GameLoop : MonoBehaviour
 {
     [SerializeField] private GameObject baseEnemyPrefab;
     [SerializeField] private GameObject armorEnemyPrefab;
-    public Waypoint firstWaypoint;
+    [SerializeField] private GameObject spawnPointRight;
+    [SerializeField] private GameObject spawnPointLeft;
+    public Waypoint startingWaypointRight;
+    public Waypoint startingWaypointLeft;
 
     public int numWaves = 5;
 
@@ -30,11 +33,13 @@ public class GameLoop : MonoBehaviour
     void InitialiseWaves(){
         enemyWaves.Add(new Dictionary<string, float>
         {
-            { "minEnemies", 12 },
-            { "maxEnemies", 16 },
+            { "minEnemies", 5 },
+            { "maxEnemies", 10 },
             { "spawnRate", 3 },
             { "enemySpeed", 3 },
-            { "waveCompletionReward", 5}
+            { "waveCompletionReward", 5},
+            {"leftSideEnemies",0}, //chance of an enemy spawning at the left side of map
+            {"armorEnemyChance",0}
         });
 
         enemyWaves.Add(new Dictionary<string, float>
@@ -43,7 +48,9 @@ public class GameLoop : MonoBehaviour
             { "maxEnemies", 15 },
             { "spawnRate", 5 },
             { "enemySpeed", 3 },
-            { "waveCompletionReward", 10}
+            { "waveCompletionReward", 10},
+            {"leftSideEnemies",0.3f},
+            {"armorEnemyChance",0}
         });
 
         enemyWaves.Add(new Dictionary<string, float>
@@ -52,7 +59,9 @@ public class GameLoop : MonoBehaviour
             { "maxEnemies", 20 },
             { "spawnRate", 4 },
             { "enemySpeed", 4 },
-            { "waveCompletionReward", 5}
+            { "waveCompletionReward", 5},
+            {"leftSideEnemies",0.4f},
+            {"armorEnemyChance",0.3f}
         });
 
         enemyWaves.Add(new Dictionary<string, float>
@@ -61,7 +70,9 @@ public class GameLoop : MonoBehaviour
             { "maxEnemies", 23 },
             { "spawnRate", 3 },
             { "enemySpeed", 4 },
-            { "waveCompletionReward", 15}
+            { "waveCompletionReward", 15},
+            {"leftSideEnemies",0.3f},
+            {"armorEnemyChance",0.4f}
         });
 
         enemyWaves.Add(new Dictionary<string, float>
@@ -70,7 +81,9 @@ public class GameLoop : MonoBehaviour
             { "maxEnemies", 26 },
             { "spawnRate", 3 },
             { "enemySpeed", 4.5f },
-            { "waveCompletionReward", 10}
+            { "waveCompletionReward", 10},
+            {"leftSideEnemies",0.5f},
+            {"armorEnemyChance",0.7f}
         });
     }
 
@@ -82,7 +95,7 @@ public class GameLoop : MonoBehaviour
 
         Debug.Log("WAVES: Starting Waves");
         while(currentWave < numWaves){
-            yield return StartCoroutine(SpawnWave(currentWave));
+            yield return StartCoroutine(SpawnWave());
 
             //wait for all enemies to die before starting next wave
             yield return new WaitUntil(() => activeEnemies.Count == 0);
@@ -103,28 +116,57 @@ public class GameLoop : MonoBehaviour
     }
 
 
-    private IEnumerator SpawnWave(int waveIndex){
-        Dictionary<string,float> waveData = enemyWaves[waveIndex];
+    private IEnumerator SpawnWave(){
+        Dictionary<string,float> waveData = enemyWaves[currentWave];
+        int numEnemies = Random.Range((int)waveData["minEnemies"],(int)waveData["maxEnemies"]+1);
+        Debug.Log($"WAVES: Starting wave {currentWave} with {numEnemies} enemies");
 
-        int numEnemies = Random.Range((int)waveData["minEnemies"],(int)waveData["maxEnemies"]);
-
-        Debug.Log($"WAVES: Starting wave {waveIndex} with {numEnemies} enemies");
+        //dynamic wave phases
+        int burstSize = Random.Range(3,5);//number of enemies spawned in a "burst"  
+        float burstDelay = waveData["spawnRate"] *0.4f;
+        float cooldownTime = Random.Range(2f,5f);
 
         for(int i=0;i<numEnemies;i++){
-            SpawnEnemy(waveData["enemySpeed"]);
-            yield return new WaitForSeconds(waveData["spawnRate"]);
+
+            //burst
+            for(int j=0;j<burstSize;j++){
+                SpawnEnemy(waveData);
+                yield return new WaitForSeconds(burstDelay);
+            }
+            yield return new WaitForSeconds(cooldownTime);
+            
+            burstSize = Random.Range(3,5);
+            cooldownTime = Random.Range(2f,4f);
         }
     }
 
-    void SpawnEnemy(float enemySpeed) 
+    void SpawnEnemy(Dictionary<string,float> waveData) 
     { 
-        GameObject gameObject = Instantiate(armorEnemyPrefab, 
-        transform.position, transform.rotation); 
-        EnemyController enemy = gameObject.GetComponent<EnemyController>(); 
 
-        enemy.waypoint = firstWaypoint; 
+        //randomly choose starting point
+        GameObject spawnPoint;
+        float leftChance = waveData["leftSideEnemies"];
+
+        if(Random.Range(0f,1f) > leftChance){
+            spawnPoint = spawnPointRight;
+        }
+        else{
+            spawnPoint = spawnPointLeft;
+        }
+        
+        //now randomly choose enemy type based on armor probability
+
+        GameObject enemyObject = Instantiate(
+            Random.Range(0f, 1f) > waveData["armorEnemyChance"] ? baseEnemyPrefab : armorEnemyPrefab,
+            spawnPoint.transform.position, 
+            spawnPoint.transform.rotation
+        );
+
+        EnemyController enemy = enemyObject.GetComponent<EnemyController>(); 
+
+        enemy.waypoint = startingWaypointRight; 
         enemy.OnEnemyDied += HandleEnemyDeath; //subsribe to the event of death 
-        enemy.SetSpeed(enemySpeed);
+        enemy.SetSpeed(waveData["enemySpeed"]);
         activeEnemies.Add(enemy);
     }
 
